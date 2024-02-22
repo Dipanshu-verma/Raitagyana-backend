6c1df0c10a5f3fbe6ff4bcf2ba494ed01c2b4afa
Dipanshu-verma <vermadipanshu444@gmail.com>
2024-02-22 17:46:44 +0530

    cors

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ require("dotenv").config();
 const app = express();
 
 app.use(express.json());
-
+app.use(cors());
 app.post("/subscribe", async (req, res) => {
     const { email } = req.body;
     if (!email) {
